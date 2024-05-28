48877db36c7223dfebdff7c4f145fb46bf9820cc
jai sharma <sharmajai901@gmail.com>
2024-05-28 23:19:47 +0530

    added the right sidebar and card section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,24 +1,47 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "Jai" };
+  const loggedIn = {
+    $id: "id",
+    email: "jai@test.com",
+    userId: "12345",
+    dwollaCustomerUrl: "test",
+    dwollaCustomerId: "test",
+    firstName: "jai",
+    lastName: "sharma",
+    address1: "BH-16",
+    city: "meerut",
+    state: "up",
+    postalCode: "250110",
+    dateOfBirth: "17/10/1999",
+    ssn: "string",
+  };
 
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || "Guest"}
-          subtext="Access and manage your account and transactions efficiently"
-        />
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250000.35}
-        />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250000.35}
+          />
+        </header>
+        RECENT TRANSACTION
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 1583.47 }]}
+      />
     </section>
   );
 };
